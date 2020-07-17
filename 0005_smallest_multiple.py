84d81481891567a65203524a9faeab8bc9f13012
Michal Kuspit <michal.kuspit@gmail.com>
2020-07-17 12:48:47 +0400

    Add files for problems #13, #14, #15

--- 0005_smallest_multiple.py
+++ 0005_smallest_multiple.py
@@ -7,9 +7,19 @@ https://projecteuler.net/problem=5
 What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 """
 #%% Solution
+def list_prime_factors(my_number):
+    prime_factors = []
+    i = 2
+    while my_number != 1:
+        while my_number % i != 0:
+            i += 1
+        my_number /= i
+        prime_factors.append(i)
+        i = 2
+    return prime_factors
 
 
-
-
+for n in range(2, 20):
+    print(list_prime_factors(n))
 
 # Answer:
